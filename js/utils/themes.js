cc3f124cd8d2356f7625095aaf9fe0de1d01a54d
glitchyfishys <152113769+glitchyfishys@users.noreply.github.com>
2024-09-01 12:43:17 +1200

    Update themes.js

--- js/utils/themes.js
+++ js/utils/themes.js
@@ -1,5 +1,5 @@
 // ************ Themes ************
-var themes = ["default", "aqua"]
+var themes = ["default", "aqua", "crimson", "coruption", "glitch"]
 
 var colors = {
 	default: {
@@ -22,6 +22,36 @@ var colors = {
 		background: "#001f3f",
 		background_tooltip: "rgba(0, 15, 31, 0.75)",
 	},
+	crimson: {
+		1: "#ed143d",
+		2: "#c51027",
+		3: "#940514",
+		color: "crimson",
+		points: "#e35f30",
+		locked: "#a43713",
+		background: "#250a04",
+		background_tooltip: "rgba(35, 5, 21, 0.75)",
+	},
+	coruption: {
+		1: "#ef00ff",
+		2: "#af00cf",
+		3: "#2f006f",
+		color: "#bf00ff",
+		points: "#b33fd5",
+		locked: "#a43713",
+		background: "#230044",
+		background_tooltip: "rgba(5, 5, 41, 0.75)",
+	},
+	glitch: {
+		1: "#00ff00",
+		2: "#00aa00",
+		3: "#005500",
+		color: "#00bb00",
+		points: "#00ff00",
+		locked: "#005500",
+		background: "#002200",
+		background_tooltip: "rgba(0, 50, 0, 0.75)",
+	},
 }
 function changeTheme() {
 
@@ -37,15 +67,15 @@ function getThemeName() {
 }
 
 function switchTheme() {
-	let index = themes.indexOf(options.theme)
+	let index = themes.indexOf(options.theme);
 	if (options.theme === null || index >= themes.length-1 || index < 0) {
 		options.theme = themes[0];
 	}
 	else {
 		index ++;
 		options.theme = themes[index];
-		options.theme = themes[1];
 	}
+	console.log(index)
 	changeTheme();
 	resizeCanvas();
 }
